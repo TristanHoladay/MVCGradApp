c31de4bbf30935a7ecc2d6e54eb804d31069e10c
TristanHoladay <tholaday777@gmail.com>
2019-09-12 19:53:33 -0600

    Company Repository and Company Service

---
+++ GradAppAPI.Core/Services/CompanyService.cs
@@ -0,0 +1,70 @@
+﻿using GradAppAPI.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradAppAPI.Core.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ICompanyRepository _companyRepo;
+
+        public CompanyService(ICompanyRepository companyRepository)
+        {
+            _companyRepo = companyRepository;
+        }
+
+        public IEnumerable<Company> GetAll()
+        {
+            IEnumerable<Company> companies = _companyRepo.GetAll();
+
+            if(companies == null)
+            {
+                throw new ApplicationException("There are no companies to display.");
+            }
+
+            return companies;
+        }
+
+        public Company Get(int id)
+        {
+            Company company = _companyRepo.Get(id);
+
+            if(company == null)
+            {
+                throw new ApplicationException("This company does not exist.");
+            }
+
+            return company;
+        }
+
+        public Company Add(Company newCompany)
+        {
+            Company company = _companyRepo.Add(newCompany);
+
+            if(company == null)
+            {
+                throw new ApplicationException("This company already exists. You can make duplicates of the same company.");
+            }
+
+            return company;
+        }
+
+        public Company Update(Company updatedCompany)
+        {
+            Company company = _companyRepo.Add(updatedCompany);
+
+            if(company == null)
+            {
+                throw new ApplicationException("The company you are trying to update does not exist.");
+            }
+
+            return company;
+        }
+
+        public void Delete(int id)
+        {
+            _companyRepo.Delete(id);
+        }
+    }
+}
--- GradAppAPI.Infrastructure/Data/CompanyRepository.cs
+++ GradAppAPI.Infrastructure/Data/CompanyRepository.cs
@@ -1,7 +1,9 @@
 ﻿using GradAppAPI.Core.Models;
 using GradAppAPI.Core.Services;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradAppAPI.Infrastructure.Data
@@ -17,27 +19,70 @@ namespace GradAppAPI.Infrastructure.Data
 
         public IEnumerable<Company> GetAll()
         {
+            IEnumerable<Company> companies = _dbContext.Companies.ToList();
 
+            if (companies == null)
+                return null;
+
+            return _dbContext.Companies
+                .Include(c => c.Users)
+                .Include(c => c.VehicleFleet)
+                .Include(c => c.ResourcesTypes)
+                .ToList();
         }
 
         public Company Get(int id)
         {
+            Company company = _dbContext.Companies.FirstOrDefault(c => c.Id == id);
 
+            if (company == null)
+                return null;
+
+            return _dbContext.Companies
+                .Include(c => c.Users)
+                .Include(c => c.VehicleFleet)
+                .Include(c => c.ResourcesTypes)
+                .FirstOrDefault(c => c.Id == id);
         }
 
         public Company Add(Company newCompany)
         {
+            Company company = _dbContext.Companies.FirstOrDefault(c => c.Id == newCompany.Id);
 
+            if (company == null)
+                return null;
+
+            _dbContext.Companies.Add(newCompany);
+            _dbContext.SaveChanges();
+
+            return newCompany;
         }
 
         public Company Update(Company updatedCompany)
         {
+            Company currentCompany = _dbContext.Companies.FirstOrDefault(c => c.Id == updatedCompany.Id);
 
+            if (currentCompany == null)
+                return null;
+
+            _dbContext.Entry(currentCompany)
+                .CurrentValues
+                .SetValues(updatedCompany);
+            _dbContext.Companies.Update(currentCompany);
+
+            _dbContext.SaveChanges();
+
+            return currentCompany;
         }
 
-        void Delete(int id)
+        public void Delete(int id)
         {
+            Company delCompany = Get(id);
 
+            if (delCompany == null) return;
+
+            _dbContext.Companies.Remove(delCompany);
+            _dbContext.SaveChanges();
         }
 
     }
